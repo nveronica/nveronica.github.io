08225327049307d11ae38fd591220824b9b04960
Your Name <you@example.com>
2021-11-20 08:02:10 +0100

    index.html

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-$document.ready("index.html"){
+// //modal show/hide
+// $(document).ready(function(){
+//     $("#modal").modal('show');
 
-}
+//     $("#hide-modal").click(function(){
+//         $("#modal").modal('hide');
+//     });
+// });
 $('.navbar a').on('click', function(e){
     if(this.hash !== ''){
         e.preventDefault();
@@ -10,18 +15,6 @@ $('.navbar a').on('click', function(e){
         $('html, body').animate(
             {
             scrollTop: $(hash).offset().top
-        },750);
-    }
-
-$('.navbar a').on('click', function(e){
-    if(this.hash !== ''){
-        e.preventDefault();
-
-        const hash = this.hash;
-
-        $('html, body').animate(
-            {
-            scrollTop: $(hash).offset().top
-        },750);
+        },75);
     }
 });
